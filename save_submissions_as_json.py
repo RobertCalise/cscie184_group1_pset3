b2c05eb3fdc748eefa3435f7bfd93e04c8f64732
Robert Calise <rcalise@rebootmotion.com>
2025-03-03 15:24:14 -0500

    Rename script; only save submissions as json

--- save_submissions_as_json.py
+++ save_submissions_as_json.py
@@ -1,12 +1,9 @@
-""" Export comment & submissions Supabase tables to JSON files. """
+""" Export submissions Supabase tables to JSON files. """
 
 from redditharbor.utils import download
 
 from modules.config import DB_CONFIG, SUPABASE_CLIENT
 
-download.comment(supabase_client=SUPABASE_CLIENT, db_name=DB_CONFIG["comment"]).to_json(
-    columns="all", file_name="comments"
-)
 download.submission(
     supabase_client=SUPABASE_CLIENT, db_name=DB_CONFIG["submission"]
 ).to_json(columns="all", file_name="submissions")
